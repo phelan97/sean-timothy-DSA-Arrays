bbed2d8c78b3fdf242f23bc2af93afe3c7f6f0c3
Timothy <superfluous11@gmail.com>
2018-11-07 08:46:13 -0800

    completed array class

--- array.js
+++ array.js
@@ -4,7 +4,7 @@ const memory = new Memory();
 
 class Array {
   constructor() {
-    this.INITIAL_CAPACITY = 20;
+    this.INITIAL_CAPACITY = 2;
     this.capacity = this.INITIAL_CAPACITY;
     this.start = memory.allocate(this.INITIAL_CAPACITY);
     this.length = 0;
@@ -12,15 +12,24 @@ class Array {
 
   // TODO: resizing
   push(val) {
+    if(this.length >= capacity) this.resize();
     const newPtr = this.start + this.length;
     memory.set(newPtr, val);
     this.length++;
   }
 
   pop() {
+    if(this.length <= 0) return null;
     const lastElementPtr = this.start + this.length - 1;
     this.length--;
     return memory.get(lastElementPtr);
+  }
+
+  resize(){
+    const oldStart = this.start;
+    this.capacity = this.capacity * 2;
+    this.start = memory.allocate(this.capacity);
+    memory.copy(this.start, oldStart, this.length);
   }
 }
 
@@ -29,6 +38,11 @@ function main() {
   array.push(22);
   array.push(33);
   array.push(44);
+  console.log(array.pop());
+  console.log(array.pop());
+  console.log(array.capacity);
+  array.resize();
+  console.log(array.capacity);
   console.log(array.pop());
   console.log(array.pop());
 }
